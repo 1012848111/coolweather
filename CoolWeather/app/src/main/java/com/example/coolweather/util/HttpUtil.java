4be8c7dd26c63dcf4cd2ca08311c76dcf7f6824f
sunfuze <sunfuze123@126.com>
2019-11-19 17:53:22 +0800

    添加一个android9网络请求权限

--- CoolWeather/app/src/main/java/com/example/coolweather/util/HttpUtil.java
+++ CoolWeather/app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -1,7 +1,14 @@
 package com.example.coolweather.util;
 
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class HttpUtil {
     public static void sendOkHttpRequest(String address,okhttp3.Callback callBack){
@@ -9,6 +16,22 @@ public class HttpUtil {
         Request request = new Request.Builder()
                 .url(address)
                 .build();
+//        Call call = client.newCall(request);
+//        //第四步:异步get请求
+//        call.enqueue(new Callback() {
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//                Log.i("onFailure", e.getMessage());
+//            }
+//
+//            @Override
+//            public void onResponse(Call call, Response response) throws IOException {
+//                //得到的子线程
+//                String result = response.body().string();
+//                Log.i("result", result);
+//            }
+//        });
+
         client.newCall(request).enqueue(callBack);
     }
 }
